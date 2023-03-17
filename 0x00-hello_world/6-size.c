a1feb99a970b494a9d8b5e135c72453f2567d95a
idrissjuanito <idrissjuanito@gmail.com>
2023-03-17 02:41:26 +0000

    corrected some more linting errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,13 @@ int main(void)
 {
 
 	long int num = 42;
-	long long int num2= 324;
+	long long int num2 = 324;
 
-	printf("size of a char: %i byte(s)\n", sizeof(char));
-	printf("size of an int: %i byte(s)\n", sizeof(int));
-	printf("size of a long int: %i byte(s)\n", sizeof(num));
-	printf("size of a long long int: %i byte(s)\n", sizeof(num2));
-	printf("size of a float: %i byte(s)\n", sizeof(float));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(num));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(num2));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 
 	return (0);
 }
