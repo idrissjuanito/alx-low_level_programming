077d39a6cfbf8d35b8df2725bcadf7c3695fd0f2
idrissjuanito <idrissjuanito@gmail.com>
2023-03-20 18:10:01 +0000

    answered last advanced question

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: alwayss 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = i; k < 10; k++)
+			{
+				for (l = j + 1; l < 10; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if ((i + j + k + l) >= 35)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
