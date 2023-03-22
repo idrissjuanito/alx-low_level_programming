459e326224f4dfa31746a0b863f5498a95b98e14
idrissjuanito <idrissjuanito@gmail.com>
2023-03-22 23:27:41 +0000

    attempted advanced Q2

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int sum;
+	int i;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("the sum of all multiples of either 3 or 5 is %d\n", sum);
+}
