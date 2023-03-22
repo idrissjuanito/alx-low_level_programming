a6d9b7c31894732d016e3e5ae6cc6d4dbd6aa598
idrissjuanito <idrissjuanito@gmail.com>
2023-03-22 00:30:33 +0000

    correction Q7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,15 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n < 0 ? (n * -1) % 10 : n % 10;
+	int ld;
 
+	if (n < 0)
+	{
+		n++;
+		n *= -1;
+		ld = (n % 10) + 1;
+	} else
+		ld = n % 10;
 	_putchar(ld + '0');
 	return (ld);
 }
