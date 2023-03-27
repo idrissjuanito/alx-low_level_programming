360b4ed8efaad961d2adacec1127bd05dee5891f
idrissjuanito <idrissjuanito@gmail.com>
2023-03-27 20:36:34 +0000

    attempted Q4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * 4-print_rev.c - prints a string in reverse order
+ *
+ * @s: string to work with
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	char *start = s;
+
+	while (*s != '\0')
+	{
+		s++;
+	}
+	do
+	{
+		_putchar(*s);
+		s--;
+	} while (s != start - 1);
+	_putchar('\n');
+}
