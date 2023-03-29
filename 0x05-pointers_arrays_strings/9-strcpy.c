814711e090c6748220d4fc647a7d73177d3cfe93
idrissjuanito <idrissjuanito@gmail.com>
2023-03-29 00:20:47 +0000

    attempted Q9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _strcopy
+ *
+ * @dest: The destination of the string to copy
+ * @src: The source string to copy
+ *
+ * Return: returns the address of the dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		*(dest + i) = src[i];
+	}
+	dest[i] = '\0';
+	return dest;
+}
