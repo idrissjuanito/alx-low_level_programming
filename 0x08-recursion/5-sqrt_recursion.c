822f8ea72714d8400fe1c4330726db4ee1e3bc98
idrissjuanito <idrissjuanito@gmail.com>
2023-04-06 16:34:05 +0000

    Another attempt at Q5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+int helper(int n, int y);
+int helpy(int n);
+
 /**
  * _sqrt_recursion - calculates the square root of of a number
  *
@@ -9,8 +12,40 @@
  */
 int _sqrt_recursion(int n)
 {
-	int *sq = 1;
+	int y = helpy(n / 2);
 
-	if  (*sq * *sq == n)
-		return (*sq);
+	return (helper(n, y));
+}
+/**
+ * helper - helps keep the value of na and calculates its square root
+ *
+ * @n: the number to get square root
+ * @y: the current root
+ *
+ * Return: the square root
+ */
+int helper(int n, int y)
+{
+	if (n == y * y)
+		return (y);
+	if (n < 0 || y * y > n)
+		return (-1);
+	return	(helper(n, y + 1));
+}
+
+/**
+ * helpy - helps does binary search on n for best y value
+ *
+ * @n: initial value of y
+ *
+ * Return: interger
+ */
+int helpy(int n)
+{
+	if (n * n <= n)
+	{
+		return (n);
+	}
+	n /= 2;
+	return	(helpy(n));
 }
