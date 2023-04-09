17191323848409d7cea5f0fceb28eb8be8539b3d
idrissjuanito <idrissjuanito@gmail.com>
2023-04-09 22:51:17 +0000

    another attempt at Q4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char **argv)
 {
-	int sum = 0, facto, i;
+	int sum = 0, facto = 0, i;
 
 	if (argc < 2)
 	{
@@ -19,9 +19,18 @@ int main(int argc, char **argv)
 	}
 	for (i = 1; i < argc; i++)
 	{
+		int sign = 1;
+
 		while (*argv[i] != '\0')
 		{
 			char c = *argv[i];
+
+			if (c == '-')
+			{
+				sign *= -1;
+				argv[i]++;
+				continue;
+			}
 
 			if (!(c >= 48 && c <= 57))
 			{
@@ -33,6 +42,7 @@ int main(int argc, char **argv)
 			facto += *argv[i] - '0';
 			argv[i]++;
 		}
+		facto *= sign;
 		sum += facto;
 		facto = 0;
 	}
