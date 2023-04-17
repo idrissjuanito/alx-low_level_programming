3dce6689a423ae47ebe63e159f98790a04c3067d
idrissjuanito <idrissjuanito@gmail.com>
2023-04-17 20:52:04 +0000

    correction on Q2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,11 +10,9 @@
  *
  * Return: void
  */
-void init_dog(dog *d, char *name, float age, char *owner)
+init_dog(dog * d, char *name, float age, char *owner)
 {
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	
-	return;	
 }
