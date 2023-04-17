924e3f7fbdbd69b671790d665a1441bf7a45ac54
idrissjuanito <idrissjuanito@gmail.com>
2023-04-17 19:35:57 +0000

    answered Q1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - details for a dig
+ * @name: name of dog
+ * @owner: onwer of the dog
+ * @age: age of the said dog
+ */
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} dog;
+#endif
