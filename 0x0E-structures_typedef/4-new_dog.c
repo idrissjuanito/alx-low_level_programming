c9519ef430950e807d8d9b75a7a3ec9713a1f079
idrissjuanito <idrissjuanito@gmail.com>
2023-04-17 23:19:06 +0000

    function for   a  new struct object

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,12 +13,21 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *bingo = malloc(sizeof(dog_t));
+	char *n = malloc(sizeof(dog_t->name));
+	char *o = malloc(sizeof(dog_t->owner));
+	int i, j;
 
-	if (bingo == NULL)
+	if (bingo == NULL || n == NULL || o == NULL)
 		return (NULL);
-	bingo->name = name;
+	for (i = 0, j = 0; name[i] != '\0'; i++, j++)
+		n[j] = name[i];
+	n[j] = '\0';
+	for (i = 0, j = 0; owner[i] != '\0'; i++, j++)
+		o[j] = owner[i];
+	o[j] = '\0';
+	bingo->name = n;
 	bingo->age = age;
-	bingo->owner = owner;
+	bingo->owner = o;
 
 	return (bingo);
 }
