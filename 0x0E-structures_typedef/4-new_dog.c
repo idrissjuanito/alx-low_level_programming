33176e39d2393be81c6803eed2945882722a2abc
idrissjuanito <idrissjuanito@gmail.com>
2023-04-17 21:53:41 +0000

    Q4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,12 +13,31 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *bingo = malloc(sizeof(dog_t));
+	char *n = malloc(sizeof(*name));
+	char *o = malloc(sizeof(*owner));
+	int i = 0, j = 0;
 
-	if (!bingo)
+	if (bingo == NULL || n == NULL || o == NULL)
 		return (NULL);
-	bingo->name = name;
+	while (name[j] != '\0')
+	{
+		n[i] = name[j];
+		i++;
+		j++;
+	}
+	n[i] = '\0';
+	i = 0;
+	j = 0;
+	while (o[i] != '\0')
+	{
+		o[i] = owner[j];
+		i++;
+		j++;
+	}
+	o[i] = '\0';
+	bingo->name = n;
 	bingo->age = age;
-	bingo->owner = owner;
+	bingo->owner = o;
 
 	return (bingo);
 }
