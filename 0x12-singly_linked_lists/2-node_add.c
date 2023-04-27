363cb742f55fac9ad55f41830ed70f8cb407aec5
idrissjuanito <idrissjuanito@gmail.com>
2023-04-27 20:09:54 +0000

    attempt 1 at Q2 0x12

---
+++ 0x12-singly_linked_lists/2-node_add.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - adds a node ot a linked list
+ *
+ * @head: pointer to the first node
+ * @str: string to add to node
+ *
+ * Return: address of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *myStr = strdup(str);
+	size_t length = strlen(myStr);
+	list_t *new = malloc(sizeof(list_t));
+
+	if (!new)
+		return (NULL);
+	new->str = myStr;
+	new->len = length;
+	new->next = *head;
+	head = &new;
+
+	return (new);
+}
