ce27fb126dc44fcae9a55cbdfe3ec4e4e0fd4dfd
idrissjuanito <idrissjuanito@gmail.com>
2023-05-02 00:02:31 +0000

    attempt 3 at Q12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -22,7 +22,7 @@ size_t print_listint_safe(const listint_t *head)
 		if (current->next > current)
 		{
 			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			exit(98);
+			break;
 		}
 		current = current->next;
 	}
