9be1dbc1da6749fa28d181189684a0c6a3427dda
idrissjuanito <idrissjuanito@gmail.com>
2023-05-05 02:12:42 +0000

    attempt 4 at Q2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,8 +13,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned int i;
 	int bit = -1;
 
-	if (n == 0)
-		return (bit);
 	for (i = 0; n > 0;  i++)
 	{
 		bit = n % 2;
@@ -23,7 +21,5 @@ int get_bit(unsigned long int n, unsigned int index)
 			return (bit);
 		n /= 2;
 	}
-	if (index >= i)
-		return (0);
 	return (bit);
 }
