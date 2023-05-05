4e4f73d3f9e4b6cb89e9ac561735fa23ba54545c
idrissjuanito <idrissjuanito@gmail.com>
2023-05-05 01:57:53 +0000

    attempt 1 at Q2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit at a particular index
+ *
+ * @n: integer to scan
+ * @index: index of the bit to find
+ *
+ * Return: the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+	int bit;
+
+	for (i = 0; n > 0;  i++)
+	{
+		bit = n % 2;
+
+		if (i == index)
+			return (bit);
+		n /= 2;
+	}
+	return (-1);
+}
