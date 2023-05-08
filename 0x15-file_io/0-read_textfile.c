cdd42bf90081942f881a7613fac8847d104df39f
idrissjuanito <idrissjuanito@gmail.com>
2023-05-08 19:20:28 +0000

    attempt 2 at Q0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,6 +20,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t r_num, w_num, rw_num = 0;
 	char buf[BUF_SIZE];
 
+	if (filename)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
