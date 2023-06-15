1d47dd33e9802152c5319d5a94acfb4c6742804c
idrissjuanito <idrissjuanito@gmail.com>
2023-06-15 16:44:37 +0000

    correction task 03

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,7 +24,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		new->prev = NULL;
 		*head = new;
-		return new;
+		return (new);
 	}
 	while (current->next)
 		current = current->next;
