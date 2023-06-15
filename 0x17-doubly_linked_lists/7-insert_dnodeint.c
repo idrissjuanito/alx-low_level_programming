d7e7090ae9e6a1c5d812f97e0b492ca3fa7b5c61
idrissjuanito <idrissjuanito@gmail.com>
2023-06-15 23:27:57 +0000

    correction task08 6

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
+	if (!current)
+		return (NULL);
 	if (!current->next)
 		return (add_dnodeint_end(h, n));
 	new = malloc(sizeof(dlistint_t));
@@ -33,6 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	new->next = current->next;
+	current->next->prev = new;
 	current->next = new;
 	new->prev = current;
 	return (new);
